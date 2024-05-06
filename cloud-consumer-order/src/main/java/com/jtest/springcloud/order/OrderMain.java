9e74fa608a3218db415097a50289816c650f66d7
rootmax1688 <aixue1688@126.com>
2024-05-06 19:55:58 +0800

    discovery

--- cloud-consumer-order/src/main/java/com/jtest/springcloud/order/OrderMain.java
+++ cloud-consumer-order/src/main/java/com/jtest/springcloud/order/OrderMain.java
@@ -1,10 +1,13 @@
 package com.jtest.springcloud.order;
 
+import com.jtest.springclould.myrule.MyselfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@RibbonClient(name = "jtest-payment",configuration = MyselfRule.class)
 public class OrderMain {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain.class,args);
